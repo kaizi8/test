2f617cbb8a17f7fe2fd1fda8d66ece800e118244
kai <592448465@qq.com>
2018-12-03 19:52:20 +0800

    111

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -14,7 +14,6 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
-
     }
 	@Test
 	public void findAll(){
