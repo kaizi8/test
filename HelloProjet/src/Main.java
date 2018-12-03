a18bb9768f961327507bfefe258d58f8fd9f9d7e
kai <592448465@qq.com>
2018-12-03 19:15:08 +0800

    更新内容

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,15 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
     }
+	@Test
+	public void findAll(){
+		System.out.println("wangwexiao");
+		System.out.println("王二小");
+		System.out.println("王文攀");
+		System.out.println("攀攀文");
+		
+	}
 }
